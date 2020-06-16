e585eb2d7bc21c818cb4e8bed41182761ae92b93
atharvakadam <atharvakadam@gmail.com>
2020-06-15 21:21:58 -0700

    Finished implementing simple redux implementation for the counter app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 import './App.css';
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1  
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>COUNTER APP</h1>
+       <div>
+        {store.getState()}
+       </div>
+       <div>
+        <button onClick={e => store.dispatch({ type: 'INCREMENT' })}>plus</button>
+        <button onClick={e => store.dispatch({ type: 'DECREMENT' })}>minus</button>
+        <button onClick={e => store.dispatch({ type: 'ZERO' })}>zero</button>
+       </div>
       </header>
     </div>
   );
 }
 
-export default App;
+
+// module.exports = {App, store}
+export {App , store};
+// export store;
